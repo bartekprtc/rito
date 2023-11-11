e1a1edf83070fa3267feee483eb82c58eb18e4fe
bartekprtc <bartlomiej.jaszczak@proton.me>
2023-11-11 15:18:19 +0000

    Deploying to gh-pages from @ bartekprtc/rito@c75ef88e6463f4186a9cd1728af89a68a5f1c41a 🚀

--- search/all_2.js
+++ search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['call_2',['call',['../classrito_1_1Lcu__wamp__handler.html#ad8cacbd983b5b168d60b16a1296a0a0f',1,'rito::Lcu_wamp_handler']]],
+  ['call_2',['call',['../classrito_1_1Lcu__wamp__handler.html#ad8cacbd983b5b168d60b16a1296a0a0f',1,'rito::Lcu_wamp_handler::call(const std::string &amp;id, const std::string &amp;function, Args &amp;&amp;... args) noexcept'],['../classrito_1_1Lcu__wamp__handler.html#ac35215d4e420060859a687968f3b26bb',1,'rito::Lcu_wamp_handler::call(const std::string &amp;id, const std::string &amp;function) noexcept']]],
   ['common_5ftypes_2eh_3',['common_types.h',['../common__types_8h.html',1,'']]],
   ['connection_5fcallback_4',['Connection_callback',['../namespacerito.html#a1778ee7a785dccf226a5dc2f28a46ccb',1,'rito']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['call_92',['call',['../classrito_1_1Lcu__wamp__handler.html#ad8cacbd983b5b168d60b16a1296a0a0f',1,'rito::Lcu_wamp_handler']]]
+  ['call_92',['call',['../classrito_1_1Lcu__wamp__handler.html#ad8cacbd983b5b168d60b16a1296a0a0f',1,'rito::Lcu_wamp_handler::call(const std::string &amp;id, const std::string &amp;function, Args &amp;&amp;... args) noexcept'],['../classrito_1_1Lcu__wamp__handler.html#ac35215d4e420060859a687968f3b26bb',1,'rito::Lcu_wamp_handler::call(const std::string &amp;id, const std::string &amp;function) noexcept']]]
 ];
